b5b12cb63a0cdc8d12293b140834ca9adf30fd7c
k0j1314 <rkwittmer@knox.edu>
2024-02-02 21:01:34 -0800

    insert is done, fixes to lookup and delete, working on rehash

--- cuckoo_hash_24.py
+++ cuckoo_hash_24.py
@@ -34,13 +34,16 @@ class CuckooHash24:
 
 	# you should *NOT* change any of the existing code above this line
 	# you may however define additional instance variables inside the __init__ method.
-
+	'''
 	def insert(self, key: int) -> bool:
 		# TODO
-
+		
 		curr_table = 0
+		evict_counter = 0
 
 		index_1 = self.hash_func(key, 0)
+		index_2 = self.hash_func(key, 1)
+		print(f"hash valsa are {index_1} and {index_2}")
 
 		# Check to see if the bucket is empty, if so, then add a list with the key in it
 		if self.tables[curr_table][index_1] is None:
@@ -50,74 +53,122 @@ class CuckooHash24:
 		# Check to see if the bucket is full, if not then append the key onto the list
 		if len(self.tables[curr_table][index_1]) < self.bucket_size:
 			#print(len(self.tables[curr_table][index_1]))
-			print( self.tables)
+			#print( self.tables)
 			self.tables[curr_table][index_1].append(key)
+			print(self.tables)
 			return True
 		
 		# else we start ping ponging
-		for _ in range(self.CYCLE_THRESHOLD):
-
+		while ( evict_counter <= self.CYCLE_THRESHOLD):
+			
 			# Check to see if bucket is empty, if so, then add a list with the key in it (second table)
 			if self.tables[curr_table][index_1] is None:
 				self.tables[curr_table][index_1] = [key]
 				return True
 			
 			# Check to see if the bucket is full, if not then append the key onto the list (second table)
-			if len(self.tables[curr_table][index_1]) < self.bucket_size:
+			elif len(self.tables[curr_table][index_1]) < self.bucket_size:
 				self.tables[curr_table][index_1].append(key)
+				
+				print(self.tables)
 				return True
 
 			# Goes through the bucket and increments counter if slot in bucket is occupied
-			if len(self.tables[curr_table][index_1]) >= self.bucket_size:
+			else:
 				slot = self.get_rand_idx_from_bucket(index_1, curr_table) # Get random slot in bucket to evict
 				evicted_key = self.tables[curr_table][index_1][slot] # Set evicted key to the element in the slot
+				#print(evicted_key)
 				self.tables[curr_table][index_1].remove(evicted_key) # Remove the evicted key from the bucket
 				self.tables[curr_table][index_1].append(key) # Append the key to the bucket
 				index_1 = self.hash_func(evicted_key, 1 - curr_table) # Update index for the other table
 
 				curr_table = 1 - curr_table #flip between 0 and 1 on the hash
+				print(evicted_key)
 				key = evicted_key #Update the key
+				
+				evict_counter += 1
+				#print(self.tables)
+		
+		# LAST ITERATION:
+		if self.tables[curr_table][index_1] is None:
+			self.tables[curr_table][index_1] = [key]
+			return True
+			
+			# Check to see if the bucket is full, if not then append the key onto the list (second table)
+		elif len(self.tables[curr_table][index_1]) < self.bucket_size:
+			print(key)
+			self.tables[curr_table][index_1].append(key)
+			print(self.tables)
+			return True
 
-			# if the bucket isn't full, then append the key onto the bucket
-			else:
-				self.tables[curr_table][index_1].append(key)
+			# Goes through the bucket and increments counter if slot in bucket is occupied
+		else:
+
+			return False
+
+
+
+	'''
+		#recursive version?
+	def insert(self, key: int) -> bool:
+		index_1 = self.hash_func(key, 0)
+		index_2 = self.hash_func(key, 1)
+		#print(f'{index_1} and {index_2} fro key {key}')
+		counter = 0
+		return self.rec_insert(key, 0, counter)
+	
+	
+	def rec_insert(self, key:int, curr:int, cycleCounter :int) -> bool:
+		index_1 = self.hash_func(key, curr)
+		#print(cycles)
+		if cycleCounter > self.CYCLE_THRESHOLD+1:		
+			#print(self.tables)
+			return False
+	
+		
+# this is fine , i doubt errors are here
+		if self.tables[curr][index_1] is None:
+			self.tables[curr][index_1] = [key]
+			return True
+		#############################
+
+
+		if len(self.tables[curr][index_1]) < self.bucket_size:
+				#print(f" {len(self.tables[curr][index_1])} is the length, acutal is {self.tables[curr][index_1]}")
+				self.tables[curr][index_1].append(key)
+				
 				return True
 		
-		#after CYCLE-1 loops we do one more insert and then return false 
-		slot = self.get_rand_idx_from_bucket(index_1, curr_table)
-		evicted_key = self.tables[curr_table][index_1][slot]
-		self.tables[curr_table][index_1].remove(evicted_key)
-		self.tables[curr_table][index_1].append(key)
-		index_1 = self.hash_func(evicted_key, 1 - curr_table)
+		else:
+			slot = self.get_rand_idx_from_bucket(index_1, curr) # Get random slot in bucket to evict
+			evicted_key = self.tables[curr][index_1][slot] 
+			self.tables[curr][index_1].remove(evicted_key) # Remove the evicted key from the bucket
+			self.tables[curr][index_1].append(key) # Append the key to the bucket
+			index_1 = self.hash_func(evicted_key, 1 - curr) # Update index for the other table
+			#print(evicted_key)
+			curr= 1 - curr #flip between 0 and 1 on the hash
+			#print(self.tables)
 
-		return False
-
+			if not self.rec_insert(evicted_key, curr, cycleCounter+1):
+				return False
+			#print(self.tables)
+			return True
+		
 	def lookup(self, key: int) -> bool:
 		# TODO
 
 		index_1 = self.hash_func(key, 0)
 		index_2 = self.hash_func(key, 1)
 
-		if self.tables[0][index_1] == None:
-			return False
-		if self.tables[1][index_2] == None:
+		if self.tables[0][index_1] is None and self.tables[1][index_2] is None:
 			return False
 		
-		if self.tables[0][index_1] == key:
+		if self.tables[0][index_1] is not None and key in self.tables[0][index_1]:
 			return True
-		if self.tables[1][index_2] == key:
+		if self.tables[1][index_2] is not None and key in self.tables[1][index_2]:
 			return True
-
-		for i in range(self.bucket_size):
-			if self.tables[0][index_1][i] == key:
-				return True
-			if self.tables[1][index_2][i] == key:
-				return True
-			
+					
 		return False
-
-		
-
 
 		
 		
@@ -128,37 +179,38 @@ class CuckooHash24:
 		index_1 = self.hash_func(key, 0)
 		index_2 = self.hash_func(key, 1)
 
-		if self.tables[0][index_1] == key:
-			self.tables[0][index_1] = None
-		if self.tables[1][index_1] == key:
-			self.tables[1][index_2] = None
 
-		for i in range(self.bucket_size):
-			if self.tables[0][index_1][i] == key:
+		if self.tables[0][index_1] is not None:
+		
+			if key in self.tables[0][index_1]:
 				if len(self.tables[0][index_1]) == 1:
 					self.tables[0][index_1] = None
 				else:
 					self.tables[0][index_1].remove(key)
 				return True
-			if self.tables[1][index_2][i] == key:
+		if key in self.tables[1][index_2]:
 				if len(self.tables[1][index_2]) == 1:
 					self.tables[1][index_2] = None
 				else:
 					self.tables[1][index_2].remove(key)
-				return True
-		
-		return False
 
-		pass
 
 	def rehash(self, new_table_size: int) -> None:
 		self.__num_rehashes += 1; self.table_size = new_table_size # do not modify this line
 		# TODO
+		old_tables = self.tables # Store original table so we have a basis to rehash
+        # TODO 
+        #Create new table
+		self.tables = [[None] * new_table_size for _ in range(2)]
 
-		
+        #Rehash existing elements from old to new table
+		for table in old_tables:
+			for slot in table:
+				if slot is not None:
+					for key in slot:
 
-		pass
-
+						if key is not None:
+							self.insert(key)
 	# feel free to define new methods in addition to the above
 	# fill in the definitions of each required member function (above),
 	# and for any additional member functions you define
